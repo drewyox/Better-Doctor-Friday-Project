9784b90d95cf3884aae92facc38a63d7048ed4aa
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-03-22 10:32:41 -0700

    redid webpack

--- src/main.js
+++ src/main.js
@@ -1,18 +1,4 @@
 import 'bootstrap';
 import 'bootstrap/scss/bootstrap.scss';
-import $ from 'jquery';
 import './sass/styles.scss';
-import { DoctorsByCondition } from './webpack-template';
-
-$(document).ready(function() {
-  let docLookup = new DoctorsByCondition();
-  let promise2 = getDoctorList(name, cond);
-
-  promise2.then((response) => {
-    let body = JSON.parse(response);
-    docList = docLookup.getDoctorList(body.drinks);
-    docList.forEach( (doc) => {
-
-    });
-  });
-});
+// import $ from 'jquery';
