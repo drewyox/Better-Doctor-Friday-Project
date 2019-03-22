7b25f2322151d99af580f686b39273faf13ca37f
Andrew Yoxsimer <Yox@Andrews-MacBook-Air.local>
2019-03-22 13:38:31 -0700

    seperate some business logic to other js file.

--- src/lookup.js
+++ src/lookup.js
@@ -1,1 +1,16 @@
 
+class Usersymp {
+
+  let request = new XMLHttpRequest();
+  const url = `https://developer.betterdoctor.com/2016-03-01/&appid=API-KEY-GOES-HERE,`;
+
+  request.onreadystatechange = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  }
+
+  request.open("GET", url, true);
+  request.send();
+}
--- src/main.js
+++ src/main.js
@@ -9,18 +9,7 @@ $(document).ready(function() {
     const symptoms = $('#symptoms').val();
     $('#symptoms').val("");
 
-    let request = new XMLHttpRequest();
-    const url = `https://developer.betterdoctor.com/2016-03-01/&appid=API-KEY-GOES-HERE,`;
-
-    request.onreadystatechange = function() {
-      if (this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    }
-
-    request.open("GET", url, true);
-    request.send();
+  
 
    const getElements = function(response) {
       $('.showInfo').text(`The humidity in ${symptoms} is ${response.main.  (("What ever represents symptoms in the api code"))  }%`);
